6c4db3a9d2eea42afb78d98e2716defe68299bc3
Derrick Bray <derrickbray@yahoo.com>
2016-11-15 22:28:13 -0600

    markup style and link admin detail

---
+++ app/admin/category/detail/route.js
@@ -0,0 +1,28 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model() {
+    return [
+      {
+  name: 'Main Courses',
+  menuItems: [
+    {
+      name: 'Super Salad',
+      price: 6,
+      description: 'Cabbage, Bacon, Egg, Nori Green Goddess'
+    },
+    {
+      name: 'Buttered Noodles',
+      price: 9,
+      description: 'Miso Butter, Pepper, Hand Pulled Noodles'
+    },
+    {
+      name: 'Hot Chicken',
+      price: 12,
+      description: 'Fried Chicken Breast, Nashville Spices'
+    },
+  ]
+}
+    ]
+  }
+});
