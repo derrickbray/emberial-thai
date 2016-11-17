fc7ffdbadf6eef91ac3226fe3d74c8ae89f04896
Derrick Bray <derrickbray@yahoo.com>
2016-11-17 09:02:26 -0600

    detail to new page

--- app/router.js
+++ app/router.js
@@ -10,7 +10,7 @@ Router.map(function() {
   this.route('admin', function() {
     this.route('category', function() {
       this.route('new');
-      this.route('detail');
+      this.route('detail', { path: '/:id' });
       this.route('new-item');
     });
   });
